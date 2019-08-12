42f28451cee55a362746fd603b5fd57c34235cb2
cagurl01 <cagurl01@louisville.edu>
2019-08-12 15:51:44 -0400

    Added choice for number of characters to strip to the newly renamed right_strip.py

--- file_transformations/right_strip.py
+++ file_transformations/right_strip.py
@@ -13,24 +13,35 @@ from datetime import datetime as dt
 
 dirpath = None
 filepath = None
+charnum = '2'
 lines = []
 while True:
-    dirpath = input('\nPlease enter directory path for file to strip:\n')
+    dirpath = input('\nPlease enter source directory path for file to strip:\n')
     dirpath = dirpath.replace(os.altsep, os.sep)
     if os.path.exists(dirpath):
         while True:
-            filename = input('\nPlease enter name for text file, including extension:\n')
+            filename = input('\nPlease enter name for nonbinary source file, including extension:\n')
             filepath = os.path.join(dirpath, filename)
             if os.path.exists(filepath):
+                while True:
+                    usernum = input('\nPlease enter number of characters to strip per line (default: 2):\n')
+                    if len(usernum) > 0 and usernum.isdigit():
+                        charnum = usernum
+                        break
+                    elif len(usernum) == 0:
+                        break
+                    else:
+                        print('\nAn integer value was not entered; try again.\n\n')
                 break
             else:
                 print('\nFile does not exist in the given directory; try again.\n\n')
         break
     else:
         print('\nPath does not exist; try again.\n\n')
+pattern = r'.{' + charnum + r'}(\r?\n?)$'
 with open(filepath) as file:
     for line in file.readlines():
-        lines.append(re.sub(r'.{2}(\r?\n?)$', r'\1', line))
+        lines.append(re.sub(pattern, r'\1', line))
 newpath = os.path.join(dirpath, 'output_{}.txt'.format(dt.today().strftime('%Y%m%d%H%M%S')))
 with open(newpath, 'w') as file:
     file.writelines(lines)
